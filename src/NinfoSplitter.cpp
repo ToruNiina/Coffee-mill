4005da8c4ed460741b40e9505469222024e2cd8f
ToruNiina <niina.toru.68u@gmail.com>
2016-01-25 12:11:14 +0900

    refactor: small change NinfoSplitter

--- src/NinfoSplitter.cpp
+++ src/NinfoSplitter.cpp
@@ -3,37 +3,42 @@
  *  written by Toru Niina in Oct. 4 2015 *
  *****************************************/
 #include "NinfoSplitter.hpp"
-
 using namespace coffeemill;
 
+// @brief> split all-in-one ninfo file into one-by-one.
 int main(int argc, char *argv[])
 {
     if(!(argc == 2 || argc == 4) )
     {
-        std::cout << "Ninfo Splitter"                                                     << std::endl;
-        std::cout << "Usage: $./split [filename].ninfo [OPTIONS]"                         << std::endl;
-        std::cout << "Outputs: [filename]_[iunit1]_[iunit2].ninfo & native_info_simN.inp" << std::endl;
-        std::cout << "Option:                                                  "          << std::endl;
-        std::cout << "-sim [const int]: define the number of native_info_sim in output"   << std::endl;
-        std::cout << "                  inp file. (default: 1)"                           << std::endl;
-        return 0;
+        std::cout << "Ninfo Splitter" << std::endl;
+        std::cout << "Usage: $./split [filename].ninfo [OPTIONS]" << std::endl;
+        std::cout << "Outputs: [filename]_[iunit1]_[iunit2].ninfo & "
+                  << "native_info_simN.inp" << std::endl;
+        std::cout << "Option: " << std::endl;
+        std::cout << "-sim [integer]: "
+                  << "define the number of native_info_sim in output"
+                  << std::endl;
+        std::cout << "                inp file. (default: 1)"
+                  << std::endl;
+        return EXIT_FAILURE;
     }
 
     std::string filename(argv[1]);
-    if( filename.substr(filename.size()-6, 6) != "\x2eninfo" )
+    if(filename.substr(filename.size()-6, 6) != "\x2eninfo" )
     {
-        std::cout << "your input filename is : " << argv[1] << std::endl;
-        std::cout << "it may not be ninfo file. Please check." << std::endl;
+        std::cout << "input filename : " << argv[1]
+                  << "may not be ninfo file." << std::endl;
+        return EXIT_FAILURE;
     }
 
     int N(1);
     if(argc == 4)
     {
-        std::string op(argv[2]);
-        if(op != "--sim" && op != "-sim")
+        const std::string option(argv[2]);
+        if(option != "--sim" && option != "-sim")
         {
-            std::cout << "invalid option" << std::endl;
-            return -1;
+            std::cout << "invalid option: " << option << std::endl;
+            return EXIT_FAILURE;
         }
         N = std::stoi(std::string(argv[3]));
     }
@@ -46,10 +51,10 @@ int main(int argc, char *argv[])
 
     std::vector<WriterSptr> writers(splitter.split());
 
-    for(std::vector<WriterSptr>::iterator iter = writers.begin(); iter != writers.end(); ++iter)
+    for(auto iter = writers.begin(); iter != writers.end(); ++iter)
     {
         (*iter)->write();
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
