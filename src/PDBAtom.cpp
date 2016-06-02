887d6d9a4bee6c469f739c0c6a176adee47a360f
ToruNiina <niina.toru.68u@gmail.com>
2016-06-02 10:12:04 +0900

    change definition of PDBAtom

---
+++ src/PDBAtom.cpp
@@ -0,0 +1,85 @@
+#include "PDBAtom.hpp"
+#include <iomanip>
+
+namespace coffeemill
+{
+
+bool operator>>(const std::string& line, PDBAtom& atom)
+{
+    const std::string pref = line.substr(0,6);
+    if(pref != "ATOM  ")
+        return false;
+    atom.prefix()  = pref;
+    atom.atom_id() = std::stoi(line.substr(6, 5));
+    atom.atom()    = line.substr(12, 4);
+    atom.altloc()  = line[16];
+    atom.residue() = line.substr(17,3);
+    atom.chain_id() = line[21];
+    atom.residue_id() = stoi(line.substr(22, 4));
+    atom.icode() = line[26];
+    double x, y, z;
+    x = stod(line.substr(30, 8));
+    y = stod(line.substr(38, 8));
+    z = stod(line.substr(46, 8));
+    atom.pos() = ax::Vector3d(x,y,z);
+
+    try{atom.occupancy() = stod(line.substr(54, 6));}
+    catch(std::exception& excpt){atom.occupancy() = 0e0;}
+
+    try{atom.temperature_factor() = stod(line.substr(60, 6));}
+    catch(std::exception& excpt){atom.temperature_factor() = 0e0;}
+
+    try{atom.element() = line.substr(76,2);}
+    catch(std::exception& excpt){atom.element() = "";}
+
+    try{atom.charge() = line.substr(78,2);}
+    catch(std::exception& excpt){atom.charge() = "";}
+
+    return true;
+}
+
+std::ostream& operator<<(std::ostream& os, const PDBAtom& a)
+{
+    os << std::setw(6) << a.prefix();
+    os << std::setw(5) << a.atom_id();
+    os << " ";
+    os << std::setw(4) << a.atom();
+    os << std::setw(1) << a.altloc();
+    os << std::setw(3) << a.residue();
+    os << " ";
+    os << std::setw(1) << a.chain_id();
+    os << std::setw(4) << a.residue_id();
+    os << std::setw(1) << a.icode();
+    os << "   ";
+    os << std::setw(8) << std::fixed << std::setprecision(3)
+       << a.x();
+    os << std::setw(8) << std::fixed << std::setprecision(3)
+       << a.y();
+    os << std::setw(8) << std::fixed << std::setprecision(3)
+       << a.z();
+    os << std::setw(6) << std::fixed << std::setprecision(2)
+       << a.occupancy();
+    os << std::setw(6) << std::fixed << std::setprecision(2)
+       << a.temperature_factor();
+    os << "          ";
+    os << std::setw(2) << a.element();
+    os << std::setw(2) << a.charge();
+    return os;
+}
+
+//ATOM      1  N   ARG A   2     735.862 773.197 574.291  1.00  0.00           N 
+std::istream& operator>>(std::istream& is, PDBAtom& atom)
+{
+    while(!is.eof())
+    {
+        std::string line;
+        std::getline(is, line);
+        if(line >> atom)
+            return is;
+        else
+            continue;
+    }
+    return is;
+}
+
+}
--- src/PDBAtom.hpp
+++ src/PDBAtom.hpp
@@ -1,148 +1,73 @@
 #ifndef COFFEE_MILL_PDB_ATOM
 #define COFFEE_MILL_PDB_ATOM
+#include "../includes/math/LinearAlgebra.hpp"
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <iomanip>
 #include <string>
-#include <memory>
 
 namespace coffeemill
 {
-    class PDBAtom
-    {
-    public:
-        enum LINE_TYPE
-        {
-            ATOM,
-            TER,
-            END,
-            EMPTY,
-            UNKNOWN
-        };
 
-    public:
+class PDBAtom
+{
+  public:
+    PDBAtom(){}
+    ~PDBAtom(){}
 
-        PDBAtom();
-        ~PDBAtom();
+          std::size_t& atom_id()       {return atom_id_;}
+    const std::size_t& atom_id() const {return atom_id_;}
+          std::size_t& residue_id()       {return residue_id_;}
+    const std::size_t& residue_id() const {return residue_id_;}
+          std::string& chain_id()       {return chain_id_;}
+    const std::string& chain_id() const {return chain_id_;}
 
-        LINE_TYPE getAtomLine(std::ifstream& ifs);
-        void read_line(std::string line);
+    double  x() const {return position_[0];}
+    double& x()       {return position_[0];}
+    double  y() const {return position_[1];}
+    double& y()       {return position_[1];}
+    double  z() const {return position_[2];}
+    double& z()       {return position_[2];}
+          ax::Vector3d& pos()       {return position_;}
+    const ax::Vector3d& pos() const {return position_;}
 
-        int get_resSeq() const {return resSeq;}
-        int get_serial() const {return serial;}
-        std::string get_name() const {return name;}
-        std::string get_element() const {return element;}
-        std::string get_charge() const {return charge;}
-        std::string get_resName() const {return resName;}
-        char get_altLoc() const {return altLoc;}
-        char get_chainID() const {return chainID;}
-        char get_iCode() const {return iCode;}
-        double get_x() const {return x;}
-        double get_y() const {return y;}
-        double get_z() const {return z;}
-        double get_occupancy() const {return occupancy;}
-        double get_tempFactor() const {return tempFactor;}
+          double& temperature_factor()       {return temperature_factor_;}
+    const double& temperature_factor() const {return temperature_factor_;}
+          double& occupancy()       {return occupancy_;}
+    const double& occupancy() const {return occupancy_;}
+          std::string& atom()       {return atom_name_;}
+    const std::string& atom() const {return atom_name_;}
+          std::string& residue()       {return residue_name_;}
+    const std::string& residue() const {return residue_name_;}
+          std::string& prefix()       {return prefix_;}
+    const std::string& prefix() const {return prefix_;}
+          std::string& icode()       {return icode_;}
+    const std::string& icode() const {return icode_;}
+          std::string& element()       {return element_;}
+    const std::string& element() const {return element_;}
+          std::string& charge()       {return charge_;}
+    const std::string& charge() const {return charge_;}
+    char& altloc()       {return altloc_;}
+    char  altloc() const {return altloc_;}
 
-        void set_resSeq(int rs){resSeq = rs;}
-        void set_name(std::string n){name = n;}
-        void set_resName(std::string rn){resName = rn;}
-        void set_element(std::string el){element = el;}
-        void set_charge(std::string ch){charge = ch;}
-        void set_chainID(char cid){chainID = cid;}
-        void set_x(double _x){x = _x;}
-        void set_y(double _y){y = _y;}
-        void set_z(double _z){z = _z;}
+  private:
 
-    private:
+    char         altloc_;
+    std::size_t  atom_id_;
+    std::size_t  residue_id_;
+    double       occupancy_;
+    double       temperature_factor_;
+    std::string  atom_name_;
+    std::string  residue_name_;
+    std::string  chain_id_;
+    std::string  prefix_;
+    std::string  icode_;
+    std::string  element_;
+    std::string  charge_;
+    ax::Vector3d position_;
+};
 
-        int serial, resSeq;
-        std::string name, resName, element, charge;
-        char altLoc, chainID, iCode;
-        double x, y, z;
-        double occupancy, tempFactor;
-    };
+bool operator>>(const std::string& line, PDBAtom& atom);
+std::ostream& operator<<(std::ostream& os, const PDBAtom& a);
+std::istream& operator>>(std::istream& is, PDBAtom& atom);
 
-    PDBAtom::PDBAtom(){}
-    PDBAtom::~PDBAtom(){}
-
-    PDBAtom::LINE_TYPE PDBAtom::getAtomLine(std::ifstream& ifs)
-    {
-        std::string line;
-        getline(ifs, line);
-        if(line.empty()) return EMPTY;
-
-        if(line.substr(0, 6) != "ATOM  ")
-        {
-            if(line.substr(0,3) == "TER")
-            {
-                return TER;
-            }
-            if(line.substr(0,3) == "END")
-            {
-                return END;
-            }
-#ifdef _DEBUG
-            std::cout << "unknown line: " << line << std::endl;
-#endif //_DEBUG
-            return UNKNOWN;
-        }
-        
-        read_line(line);
-        return ATOM;
-    }
-
-    void PDBAtom::read_line(std::string line)
-    {
-        //typical example
-        // ATOM      1  O5'  DA I -73      24.126 139.747  27.198  1.00 82.07           O  
-        this->serial     = stoi(line.substr(6, 5));
-        this->name       = line.substr(12, 4);
-        this->altLoc     = line[16];
-        this->resName    = line.substr(17,3);
-        this->chainID    = line[21];
-        this->resSeq     = stoi(line.substr(22, 4));
-        this->iCode      = line[26];
-        this->x          = stod(line.substr(30, 8));
-        this->y          = stod(line.substr(38, 8));
-        this->z          = stod(line.substr(46, 8));
-        this->occupancy  = stod(line.substr(54, 6));
-        this->tempFactor = stod(line.substr(60, 6));
-        this->element    = line.substr(76,2);
-        this->charge     = line.substr(78,2);
-
-        return;
-    }
-
-    std::ostream& operator<<(std::ostream& os, const PDBAtom& a)
-    {
-        os << "ATOM  ";
-        os << std::setw(5) << a.get_serial();
-        os << " ";
-        os << std::setw(4) << a.get_name();
-        os << std::setw(1) << a.get_altLoc();
-        os << std::setw(3) << a.get_resName();
-        os << " ";
-        os << std::setw(1) << a.get_chainID();
-        os << std::setw(4) << a.get_resSeq();
-        os << std::setw(1) << a.get_iCode();
-        os << "   ";
-        os << std::setw(8) << std::fixed << std::setprecision(3)
-           << a.get_x();
-        os << std::setw(8) << std::fixed << std::setprecision(3)
-           << a.get_y();
-        os << std::setw(8) << std::fixed << std::setprecision(3)
-           << a.get_z();
-        os << std::setw(6) << std::fixed << std::setprecision(2)
-           << a.get_occupancy();
-        os << std::setw(6) << std::fixed << std::setprecision(2)
-           << a.get_tempFactor();
-        os << "          ";
-        os << std::setw(2) << a.get_element();
-        os << std::setw(2) << a.get_charge();
-        return os;
-    }
-
-    typedef std::shared_ptr<PDBAtom> AtomSptr;
 }
 #endif // COFFEE_MILL_PDB_ATOM
