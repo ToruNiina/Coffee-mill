431fd7586eff09a3c5a09c5f70055ee105418407
ToruNiina <niina.toru.68u@gmail.com>
2016-06-03 13:18:35 +0900

    follow change in pdb

--- src/SequenceExtractor.cpp
+++ src/SequenceExtractor.cpp
@@ -1,58 +1,27 @@
-#include "pdb/PDBChain.hpp"
-#include "pdb/CGChain.hpp"
+#include "PDBChain.hpp"
+#include "PDBReader.hpp"
 using namespace coffeemill;
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2 && argc != 3)
+    if(argc != 2)
     {
-        std::cout << "Usage  : ./seqextr [OPTION] [filename].pdb" << std::endl;
-        std::cout << "Option : -cg : read cg style" << std::endl;
-        return -1;
+        std::cerr << "Usage  : ./seqextr [filename].pdb" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    std::string argv1(argv[1]);
-    if(argv1 == "-cg")
-    {//read CG style
-        std::ifstream pdbfile(argv[2]);
-
-        while(!pdbfile.eof())
-        {
-            CGChain chain;
-            chain.read_block(pdbfile);
-
-            if(chain.chain_exist())
-            {
-                std::cout << "Chain " << chain.get_chainID() << ": ";
-                std::cout << chain.get_sequence() << std::endl;
-            }
-        }
+    PDBReader reader(argv[1]);
+    reader.read();
+    auto structure = reader.parse();
+    std::cout << "structure.size " << structure.size() << std::endl;
+    for(auto chain = structure.cbegin(); chain != structure.cend(); ++chain)
+    {
+        std::cout << "chain.size " << chain->size() << std::endl;
+        for(auto residue = chain->cbegin(); residue != chain->cend(); ++residue)
+            std::cout << (*residue)->residue() << std::endl;
     }
-    else
-    {//read plane PDB
-        try{
-            std::ifstream pdbfile(argv[1]);
 
-            while(!pdbfile.eof())
-            {
-                PDBChain chain;
-                chain.read_block(pdbfile);
-
-                if(chain.chain_exist())
-                {
-                    std::cout << "Chain " << chain.get_chainID() << ": ";
-                    std::cout << chain.get_sequence() << std::endl;
-                }
-            }
-        }
-        catch(std::exception& exp)
-        {
-            std::cerr << "while reading file, exception " << exp.what() << "was caught." << std::endl;
-            std::cerr << "please use -cg option to read CG PDB" << std::endl;
-            return EXIT_FAILURE;
-        }
-    }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
