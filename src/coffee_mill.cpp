97b8caafd9eb0ea8543312d80c81b56fb144870b
ToruNiina <niina.toru.68u@gmail.com>
2016-06-03 15:32:26 +0900

    add main file

---
+++ src/coffee_mill.cpp
@@ -0,0 +1,91 @@
+#include "PDBReader.hpp"
+#include "SequenceExtractor.hpp"
+#include "coffee_mill.hpp"
+#ifndef MAJOR_VERSION
+#define MAJOR_VERSION 0
+#endif
+#ifndef MINOR_VERSION
+#define MINOR_VERSION 0
+#endif
+
+int main(int argc, char *argv[])
+{
+    coffeemill::CommandLine command(argc, argv);
+    if(!command.parse())
+    {
+        command.print_help();
+        return EXIT_FAILURE;
+    }
+
+    switch(command.mode())
+    {
+      case coffeemill::CommandLine::MODE::PDB:
+        {
+        command.print_logo<MAJOR_VERSION, MINOR_VERSION>();
+
+        switch(command.job())
+        {
+          case coffeemill::CommandLine::JOB::SEQ:
+            {
+            
+            coffeemill::PDBReader reader(command.file());
+            reader.read();
+            auto structure = reader.parse();
+
+            coffeemill::SequenceExtractor seqextr;
+            for(auto chain = structure.cbegin(); chain != structure.cend(); ++chain)
+            {
+                std::cout << "chain " << chain->chain_id() << ": "
+                          << seqextr(*chain) << std::endl;
+            }
+            break;
+            }// job pdb::seq
+          case coffeemill::CommandLine::JOB::SPLIT:
+            throw std::runtime_error("not implemented yet");
+          case coffeemill::CommandLine::JOB::JOIN:
+            throw std::runtime_error("not implemented yet");
+          case coffeemill::CommandLine::JOB::COMPLEMENTAL:
+            throw std::runtime_error("invalid command");
+          case coffeemill::CommandLine::JOB::MUTATE:
+            throw std::runtime_error("not implemented yet");
+          case coffeemill::CommandLine::JOB::SHOW:
+            throw std::runtime_error("invalid command");
+          case coffeemill::CommandLine::JOB::MAKE_CG:
+            throw std::runtime_error("not implemented yet");
+          case coffeemill::CommandLine::JOB::MAKE_NINFO:
+            throw std::runtime_error("not implemented yet");
+          case coffeemill::CommandLine::JOB::MAKE_MOVIE:
+            throw std::runtime_error("invalid command");
+          default:                           
+            throw std::logic_error("never reach here");
+        }
+        break;
+        }// mode pdb
+      case coffeemill::CommandLine::MODE::DNA:
+        {
+            throw std::runtime_error("not implemented yet");
+        }
+      case coffeemill::CommandLine::MODE::DCD:
+        {
+            throw std::runtime_error("not implemented yet");
+        }
+      case coffeemill::CommandLine::MODE::NINFO:
+        {
+            throw std::runtime_error("not implemented yet");
+        }
+      case coffeemill::CommandLine::MODE::HELP:
+        {
+            command.print_help();
+            break;
+        }// mode help
+      case coffeemill::CommandLine::MODE::MANUAL:
+        {
+            command.print_man();
+            break;
+        }// mode manual
+      default:
+        throw std::logic_error("never reach here");
+    }
+
+    return EXIT_SUCCESS;
+}
