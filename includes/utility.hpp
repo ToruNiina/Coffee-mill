93869b4e14e10b047360aae6c149626b805acf70
ToruNiina <niina.toru.68u@gmail.com>
2016-06-06 17:28:15 +0900

    add util func

--- includes/utility.hpp
+++ includes/utility.hpp
@@ -14,6 +14,20 @@ inline std::string remove_all(const std::string& str)
     return retval;
 }
 
+template<class T_target, class T_reference>
+bool is_composed_of(const T_target& target, const T_reference& ref)
+{
+    for(auto iter = target.cbegin(); iter != target.cend(); ++iter)
+    {
+        bool found = false;
+        for(auto reference = ref.cbegin(); reference != ref.cend(); ++reference)
+            if(*iter == *reference) found = true;
+
+        if(!found) return false;
+    }
+    return true;
+}
+
 }
 
 #endif /* COFFEE_MILL_UTILITY */
