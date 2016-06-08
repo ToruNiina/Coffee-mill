ed9b5309a3228cc5e98ce7a2ab9ecde75a6826c4
ToruNiina <niina.toru.68u@gmail.com>
2016-06-08 12:48:19 +0900

    add some util funcs

--- includes/utility.hpp
+++ includes/utility.hpp
@@ -6,12 +6,31 @@ namespace coffeemill
 {
 
 template<char del>
-inline std::string remove_all(const std::string& str)
+std::string remove_all(const std::string& str)
 {
     std::string retval;
     for(auto iter = str.cbegin(); iter != str.cend(); ++iter)
         if(*iter != del) retval += *iter;
     return retval;
+}
+
+template<char del>
+std::string remove_front(const std::string& str)
+{
+    auto iter = str.cbegin();
+    while(*iter == del)
+        ++iter;
+    return std::string(iter, str.cend());
+}
+
+template<char del>
+std::string remove_back(const std::string& str)
+{
+    auto iter = str.crbegin();
+    while(*iter == del)
+        ++iter;
+    const std::string rev(iter, str.cend());
+    return std::string(rev.crbegin(), rev.crend());
 }
 
 template<class T_target, class T_reference>
@@ -26,6 +45,13 @@ bool is_composed_of(const T_target& target, const T_reference& ref)
         if(!found) return false;
     }
     return true;
+}
+
+inline bool is_contain(const std::string& target, const std::string& ref)
+{
+    for(std::size_t i=0; i<=target.size() - ref.size(); ++i)
+        if(target.substr(i, ref.size()) == ref) return true;
+    return false;
 }
 
 }
