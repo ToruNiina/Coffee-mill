47abc74eb2ac5f59170e10fb37eaf3297b6b6f2a
ToruNiina <niina.toru.68u@gmail.com>
2016-06-08 13:19:11 +0900

    change NinfoReader

--- src/NinfoReader.hpp
+++ src/NinfoReader.hpp
@@ -13,21 +13,30 @@ class NinfoReader
 
   public:
 
+    NinfoReader(){}
     NinfoReader(const std::string& filename)
         : filename_(filename)
     {}
     ~NinfoReader() = default;
 
     void read();
+    void read(const std::string& filename){filename_ = filename; return this->read();}
 
           std::string& filename()       {return filename_;}
     const std::string& filename() const {return filename_;}
           data_type& data()       {return data_;}
     const data_type& data() const {return data_;}
+
+  private:
+    void       read_block(std::ifstream& ifs, const std::string& line);
+    template<typename T_ninfo>
+    NinfoBlock read_block(std::ifstream& ifs) const;
+    NinfoKind  judge_block(const std::string& line) const;
   
   private:
     std::string filename_;
     data_type   data_;
+
 };
 
 }
