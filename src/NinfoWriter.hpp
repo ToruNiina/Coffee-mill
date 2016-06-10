ed117ae6b1afff3232f3ca09131312a2d2d86a9b
ToruNiina <niina.toru.68u@gmail.com>
2016-06-10 10:24:28 +0900

    add document of NinfoWriter

--- src/NinfoWriter.hpp
+++ src/NinfoWriter.hpp
@@ -1,3 +1,14 @@
+/*!
+  @file NinfoWriter.hpp
+  @brief definition of NinfoWriter class.
+
+  NinfoWriter writes NinfoData into a file.
+  
+  @author Toru Niina (niina.toru.68@gmail.com)
+  @date 2016-06-10 10:00
+  @copyright Toru Niina 2016 on MIT License
+*/
+
 #include "NinfoData.hpp"
 #include <fstream>
 
@@ -7,13 +18,25 @@ namespace coffeemill
 class NinfoWriter
 {
   public:
+    //! ctor
     NinfoWriter(){}
+    /*!
+     * @brief ctor.
+     * @param filename file to read.
+     */
     NinfoWriter(const std::string& filename): filename_(filename){}
+    //! dtor
     ~NinfoWriter() = default;
 
+    /*!
+     * @brief write data into a file
+     * @param data NinfoData to write.
+     */
     void write(const NinfoData& data) const;
 
+    //! filename
           std::string& filename()       {return filename_;}
+    //! filename
     const std::string& filename() const {return filename_;}
 
   private:
@@ -21,7 +44,7 @@ class NinfoWriter
     void write_block(std::ofstream& ofs, const NinfoBlock& block) const;
 
   private:
-    std::string filename_;
+    std::string filename_; //!< filename to write
 };
 
 }
