d4682e3543cc5bce5e31563ec28688a0b0524049
ToruNiina <niina.toru.68u@gmail.com>
2016-10-17 23:54:03 +0900

    follow change

--- includes/PDBResidue.hpp
+++ includes/PDBResidue.hpp
@@ -15,9 +15,10 @@
 #ifndef COFFEE_MILL_PDB_RESIDUE
 #define COFFEE_MILL_PDB_RESIDUE
 #include "PDBAtom.hpp"
+#include <vector>
 #include <memory>
 
-namespace coffeemill
+namespace mill
 {
 
 //! PDBResidue class.
@@ -25,91 +26,56 @@ namespace coffeemill
  * PDBResidue contains shared pointer of PDBAtom as vector. 
  * this behaves like std::vector.
  */
+template<typename vectorT>
 class PDBResidue
 {
   public:
-    using size_type      = std::size_t;
-    using index_type     = size_type;
-    using element_type   = std::shared_ptr<PDBAtom>;
-    using container_type = std::vector<element_type>;
-    using iterator       = container_type::iterator;
-    using const_iterator = container_type::const_iterator;
+    using vector_type    = vectorT;
+    using atom_type      = PDBAtom<vector_type>;
+    using container_type = std::vector<atom_type>;
+    using iterator       = typename container_type::iterator;
+    using const_iterator = typename container_type::const_iterator;
 
-  public:
-
-    //! ctor
-    PDBResidue(){}
-    /*!
-     * @brief ctor. allocate the container with size
-     * @param size number of residues. equal to size of the container.
-    */
-    PDBResidue(size_type size) : atoms_(size){}
-    //! dtor
+    PDBResidue() = default;
+    explicit PDBResidue(std::size_t size) : atoms_(size){}
     ~PDBResidue() = default;
 
-    /*!
-     * @brief residue id of this residue.
-     * NOTE: this class does not guarantee the atoms are in the same residue.
-     * @return residue id of the 1st PDBAtom.
-     */
-    size_type          residue_id() const {return atoms_.front()->residue_id();}
-    /*!
-     * @brief residue name of this residue.
-     * NOTE: this class does not guarantee the atoms are in the same residue.
-     * @return residue name of the 1st PDBAtom.
-     */
-    const std::string& residue()    const {return atoms_.front()->residue();}
-    /*!
-     * @brief chain id of this residue.
-     * NOTE: this class does not guarantee the atoms are in the same chain.
-     * @return chain id of the 1st PDBAtom.
-     */
-    const std::string& chain_id()   const {return atoms_.front()->chain_id();}
+    PDBResidue(PDBResidue const&) = default;
+    PDBResidue(PDBResidue&&) = default;
+    PDBResidue& operator=(PDBResidue const&) = default;
+    PDBResidue& operator=(PDBResidue&&) = default;
 
-    //! return whether the container is empty or not
-    bool      empty() const {return atoms_.empty();}
-    //! return the number of atoms
-    size_type size()  const {return atoms_.size();}
-    //! clear the container
-    void clear(){return atoms_.clear();}
-    /*!
-     * @brief push new residue into the container.
-     * NOTE: this method does not check whether the ATOM line is
-     *       in the same residue or not. User must guarantee the point.
-     */
-    void push_back(const element_type& atom){return atoms_.push_back(atom);}
+    int  residue_id() const {return residue_id_;}
+    int& residue_id()       {return residue_id_;}
+    std::string const& residue_name() const {return residue_name_;}
+    std::string&       residue_name()       {return residue_name_;}
+    std::string const& chain_id() const {return chain_id_;}
+    std::string &      chain_id()       {return chain_id_;}
 
-    //! access to the 1st atom
-    const element_type& front() const {return atoms_.front();}
-    //! access to the 1st atom
-          element_type& front()       {return atoms_.front();}
-    //! access to the last atom
-    const element_type& back() const {return atoms_.back();}
-    //! access to the last atom
-          element_type& back()       {return atoms_.back();}
-    //! access to the i-th member
-    const element_type& at(index_type i) const {return atoms_.at(i);}
-    //! access to the i-th member
-          element_type& at(index_type i)       {return atoms_.at(i);}
-    //! access to the i-th member
-    const element_type& operator[](index_type i) const {return atoms_[i];}
-    //! access to the i-th member
-          element_type& operator[](index_type i)       {return atoms_[i];}
+    std::size_t size() const {return atoms_.size();}
+    bool       empty() const {return atoms_.empty();}
+    void clear(){atoms_.clear();}
+    void resize(const std::size_t s){atoms_.reserve(s);}
+    void reserve(const std::size_t s){atoms_.reserve(s);}
 
-    //! iterator
+    void push_back(const atom_type& atom){atoms_.push_back(atom);}
+    void emplace_back(atom_type&& atom){atoms_.emplace_back(std::forward(atom));}
+
+    atom_type &      at(const std::size_t i)       {return atoms_.at(i);}
+    atom_type const& at(const std::size_t i) const {return atoms_.at(i);}
+    atom_type &      operator[](const std::size_t i)       {return atoms_[i];}
+    atom_type const& operator[](const std::size_t i) const {return atoms_[i];}
     iterator begin() {return atoms_.begin();}
-    //! iterator
     iterator end()   {return atoms_.end();}
-    //! iterator
     const_iterator cbegin() const {return atoms_.cbegin();}
-    //! iterator
     const_iterator cend()   const {return atoms_.cend();}
 
   private:
-    container_type atoms_;//!< array of *atoms
+    int residue_id_;
+    std::string residue_name_;
+    std::string chain_id_;
+    container_type atoms_;
 };
 
-
-
-}
+} // mill
 #endif /* COFFEE_MILL_PDB_RESIDUE */
