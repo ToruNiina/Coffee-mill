597f72c41190aa1dbd4ac53fb0bfefb24ef980c8
ToruNiina <niina.toru.68u@gmail.com>
2016-10-18 12:50:43 +0900

    add specialization of scalar_type_helper for AX

--- includes/detail/scalar_type_extractor.hpp
+++ includes/detail/scalar_type_extractor.hpp
@@ -1,5 +1,6 @@
 #ifndef COFFEE_MILL_SCALAR_TYPE_EXTRACTOR
 #define COFFEE_MILL_SCALAR_TYPE_EXTRACTOR
+#include "math/LinearAlgebra.hpp"
 
 namespace mill
 {
@@ -7,6 +8,12 @@ template<typename vectorT>
 struct scalar_type_extractor
 {
     typedef typename vectorT::scalar_type type;
+};
+
+template<typename realT, std::size_t N>
+struct scalar_type_extractor<ax::Vector<realT, N>>
+{
+    typedef realT type;
 };
 
 }
