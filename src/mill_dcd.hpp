04e4f5ca147642c53332c0277c97ff413d6703bd
ToruNiina <niina.toru.68u@gmail.com>
2016-10-22 16:26:58 +0900

    add dcd impose

--- src/mill_dcd.hpp
+++ src/mill_dcd.hpp
@@ -1,6 +1,7 @@
 #ifndef COFFEE_MILL_DCD_MODE
 #define COFFEE_MILL_DCD_MODE
 #include "DCDtoMovie.hpp"
+#include "SuperImpose.hpp"
 
 namespace mill
 {
@@ -18,6 +19,14 @@ int mode_dcd(int argument_c, char **argument_v)
                     "usage: ./mill dcd make-movie [file.dcd | file.toml]");
         std::string fname(argument_v[2]);
         return dcd_to_movie<vectorT>(fname);
+    }
+    else if(command == "impose")
+    {
+        if(argument_c != 3)
+            throw std::invalid_argument(
+                    "usage: ./mill dcd impose [file.dcd | file.toml]");
+        std::string fname(argument_v[2]);
+        return superimpose<vectorT>(fname);
     }
     else
     {
