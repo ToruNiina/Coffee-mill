761a07ccb99a44ab7fd21854ddd80a00491d935d
ToruNiina <niina.toru.68u@gmail.com>
2017-01-05 17:32:25 +0900

    add read_traj method to XYZReader

--- includes/data/XYZReader.hpp
+++ includes/data/XYZReader.hpp
@@ -31,20 +31,43 @@ class XYZReader
     using vector_type     = vectorT;
     using position_type   = vector_type;
     using real_type       = typename scalar_type_extractor<vector_type>::type;
-    using snapshot_type   = std::vector<std::pair<std::string, position_type>>;
-    using trajectory_type = std::vector<snapshot_type>;
+    using snapshot_type       = std::vector<position_type>;
+    using trajectory_type     = std::vector<snapshot_type>;
+    using xyz_snapshot_type   = std::vector<std::pair<std::string, position_type>>;
+    using xyz_trajectory_type = std::vector<xyz_trajectory_type>;
 
   public:
 
     DCDReader()  = default;
     ~DCDReader() = default;
 
-    trajectory_type read(const std::string& fname);
-    trajectory_type read(std::istream& is);
+    xyz_trajectory_type read(const std::string& fname);
+    xyz_trajectory_type read(std::istream& is);
+
+    trajectory_type read_traj(const std::string& fname);
+    trajectory_type read_traj(std::istream& is);
 
 //     snapshot_type read_snapshot(const std::string& fname, const std::size_t i);
-    snapshot_type read_snapshot(std::istream& is);
+    xyz_snapshot_type read_xyz_snapshot(std::istream& is);
+    snapshot_type     read_snapshot(std::istream& is);
 };
+
+template<typename vectorT>
+typename XYZReader<vectorT>::xyz_trajectory_type
+XYZReader<vectorT>::read(const std::string& fname)
+{
+    std::ifstream ifs(fname);
+    if(not ifs.good())
+        throw std::runtime_error("file open error: " + fname);
+
+    xyz_trajectory_type traj;
+    while(not ifs.eof())
+    {
+        traj.emplace_back(read_xyz_snapshot(ifs));
+    }
+
+    return traj;
+}
 
 template<typename vectorT>
 typename XYZReader<vectorT>::trajectory_type
@@ -64,8 +87,20 @@ XYZReader<vectorT>::read(const std::string& fname)
 }
 
 template<typename vectorT>
-typename XYZReader<vectorT>::trajectory_type
+typename XYZReader<vectorT>::xyz_trajectory_type
 XYZReader<vectorT>::read(std::istream& is)
+{
+    xyz_trajectory_type traj;
+    while(not is.eof())
+    {
+        traj.emplace_back(read_xyz_snapshot(is));
+    }
+    return traj;
+}
+
+template<typename vectorT>
+typename XYZReader<vectorT>::trajectory_type
+XYZReader<vectorT>::read_traj(std::istream& is)
 {
     trajectory_type traj;
     while(not is.eof())
@@ -76,8 +111,8 @@ XYZReader<vectorT>::read(std::istream& is)
 }
 
 template<typename vectorT>
-typename XYZReader<vectorT>::trajectory_type
-XYZReader<vectorT>::read_snapshot(std::istream& is)
+typename XYZReader<vectorT>::xyz_snapshot_type
+XYZReader<vectorT>::read_xyz_snapshot(std::istream& is)
 {
     std::string num, comment;
     std::getline(is, num);
@@ -105,6 +140,41 @@ XYZReader<vectorT>::read_snapshot(std::istream& is)
         real_type x, y, z;
         iss >> header >> x >> y >> z;
         snapshot.at(i) = std::make_pair(header, position_type(x, y, z));
+    }
+
+    return snapshot;
+}
+
+template<typename vectorT>
+typename XYZReader<vectorT>::snapshot_type
+XYZReader<vectorT>::read_snapshot(std::istream& is)
+{
+    std::string num, comment;
+    std::getline(is, num);
+    std::getline(is, comment);
+    std::size_t N;
+    try
+    {
+        if(num.empty) throw std::excpetion;
+        N = std::stoi(num);
+    }
+    catch(std::exception& except)
+    {
+        throw std::runtime_error("invalid XYZ file format");
+    }
+
+    std::string line;
+    snapshot_type snapshot(N);
+    for(std::size_t i=0; i<N; ++i)
+    {
+        if(not is.good()) throw std::runtime_error("invalid xyz file");
+
+        std::getline(is, line);
+        std::istringstream iss(line);
+        std::string header;
+        real_type x, y, z;
+        iss >> header >> x >> y >> z;
+        snapshot.at(i) = position_type(x, y, z);
     }
 
     return snapshot;
