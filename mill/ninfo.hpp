8962c626904cb3619643ef8ac8d63e16ef9c0bc8
ToruNiina <niina.toru.68u@gmail.com>
2017-03-21 22:02:00 +0900

    change path from includes to mill

--- mill/ninfo.hpp
+++ mill/ninfo.hpp
@@ -1,9 +1,9 @@
 #ifndef COFFEE_MILL_INCLUDE_NINFO
 #define COFFEE_MILL_INCLUDE_NINFO
 
-#include "includes/data/NinfoBase.hpp"
-#include "includes/data/NinfoData.hpp"
-#include "includes/data/NinfoReader.hpp"
-#include "includes/data/NinfoWriter.hpp"
+#include "data/NinfoBase.hpp"
+#include "data/NinfoData.hpp"
+#include "data/NinfoReader.hpp"
+#include "data/NinfoWriter.hpp"
 
 #endif /* COFFEE_MILL_INCLUDE_NINFO */
