6c24a5a0436d5e5e821b28d91586a7c1908b6e48
ToruNiina <niina.toru.68u@gmail.com>
2017-04-11 00:37:01 +0900

    refine implementation of mill-pdb-seq

--- src/SequenceExtractor.hpp
+++ src/SequenceExtractor.hpp
@@ -1,5 +1,5 @@
-#ifndef COFFEE_MILL_SEQUENCE_EXTRACTOR
-#define COFFEE_MILL_SEQUENCE_EXTRACTOR
+#ifndef COFFEE_MILL_PDB_SEQ
+#define COFFEE_MILL_PDB_SEQ
 #include <mill/data/PDBChain.hpp>
 #include <mill/util/string.hpp>
 #include <string>
@@ -8,22 +8,91 @@
 namespace mill
 {
 
-template<typename vectorT>
-class SequenceExtractor
+template<typename Iterator>
+struct residue_name_iterator
 {
-  public:
-    using vector_type = vectorT;
-    using chain_type = PDBChain<vector_type>;
+    residue_name_iterator(Iterator iter) : iter_(iter){}
 
-  public:
-    SequenceExtractor(){}
-    ~SequenceExtractor() = default;
+    std::string operator*() const
+    {
+        return remove_all(' ', iter_->residue_name());
+    }
 
-    std::string extract(const chain_type& chain) const;
+    residue_name_iterator& operator++()
+    {
+        ++iter_;
+        return *this;
+    }
+    residue_name_iterator& operator++(int)
+    {
+        const auto tmp = *this;
+        ++(*this);
+        return tmp;
+    }
 
-  private:
+    Iterator iter_;
+};
 
-    std::map<std::string, char> amino_acid = {
+template<typename Iterator>
+inline residue_name_iterator<Iterator>
+wrap_residue_name(Iterator iter)
+{
+    return residue_name_iterator<Iterator>(iter);
+}
+
+template<typename Iterator>
+inline bool operator==(residue_name_iterator<Iterator> const& lhs,
+                       residue_name_iterator<Iterator> const& rhs)
+{
+    return lhs.iter_ == rhs.iter_;
+}
+
+template<typename Iterator>
+inline bool operator<(residue_name_iterator<Iterator> const& lhs,
+                      residue_name_iterator<Iterator> const& rhs)
+{
+    return lhs.iter_ < rhs.iter_;
+}
+
+template<typename Iterator>
+inline bool operator!=(residue_name_iterator<Iterator> const& lhs,
+                       residue_name_iterator<Iterator> const& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template<typename Iterator>
+inline bool operator<=(residue_name_iterator<Iterator> const& lhs,
+                       residue_name_iterator<Iterator> const& rhs)
+{
+    return (lhs < rhs) || (lhs == rhs);
+}
+
+template<typename Iterator>
+inline bool operator>(residue_name_iterator<Iterator> const& lhs,
+                       residue_name_iterator<Iterator> const& rhs)
+{
+    return !(lhs <= rhs);
+}
+
+template<typename Iterator>
+inline bool operator>=(residue_name_iterator<Iterator> const& lhs,
+                       residue_name_iterator<Iterator> const& rhs)
+{
+    return !(lhs < rhs);
+}
+
+struct amino_acid_code
+{
+    char operator()(std::string const& s) const {return codes.at(s);}
+    std::string const& operator()(char s) const
+    {
+        return std::find_if(codes.cbegin(), codes.cend(),
+            [=](const std::pair<std::string, char>& p){return p.second==s;}
+            )->first;
+    }
+
+    std::map<std::string, char> codes = {
         {"ALA", 'A'},
         {"ASX", 'B'},
         {"CYS", 'C'},
@@ -51,37 +120,54 @@ class SequenceExtractor
     };
 };
 
-template<typename vectorT>
-std::string SequenceExtractor<vectorT>::extract(const chain_type& chain) const
+struct generate_sequence
 {
-    std::string sequence;
-    for(auto residue = chain.cbegin(); residue != chain.cend(); ++residue)
+    char operator()(const std::string& res) const
     {
-        std::string res = residue->residue_name();
-        res = remove_all(' ', res);
-        if(res[0] == 'D')
+        if(res.size() == 1)
         {
-            sequence += res[1];
+            return res.front();
         }
-        else if(res.size() == 1)
+        else if(res[0] == 'D' && res.size() == 2)
         {
-            sequence += res;
+            return res[1];
         }
-        else 
+        else if(res.size() == 3)
         {
-            try{sequence += amino_acid.at(res);}
-            catch(std::out_of_range& except)
-            {
-                std::cerr << "Exception caught: residue " << res << std::endl; 
-                std::cerr << "what: " << except.what() << std::endl;
-                throw;
-            }
+            return amino(res);
         }
+        else
+            throw std::invalid_argument("unknown sequence: " + res);
     }
-    return sequence;
+
+    amino_acid_code amino;
+};
+
+// argv := {"seq", "file.pdb"}
+template<typename vectorT>
+int mill_pdb_seq(int argument_c, char **argument_v)
+{
+    if(argument_c < 2)
+    {
+        std::cerr << "error: mill pdb seq: too few commands" << std::endl;
+        return 1;
+    }
+
+    const std::string pdbname(argument_v[1]);
+    PDBReader<vectorT> reader;
+    const auto chains = reader.parse(reader.read(pdbname));
+
+    for(auto&& chain : chains)
+    {
+        std::string sequence(chain.size(), ' ');
+        std::transform(wrap_residue_name(chain.cbegin()),
+                       wrap_residue_name(chain.cend()),
+                       sequence.begin(),
+                       generate_sequence());
+        std::cout << "chain " << chain.chain_id() << ": " << sequence << std::endl;
+    }
+    return 0;
 }
 
-
 } // mill
-
-#endif /* COFFEE_MILL_SEQUENCE_EXTRACTOR */
+#endif /* COFFEE_MILL_PDB_SEQ */
