d4dcf37813c7250cc3227a0e56ae4eddcfcd58e5
ToruNiina <niina.toru.68u@gmail.com>
2017-12-21 14:51:42 +0900

    fix typoes

--- mill/util/is_all.hpp
+++ mill/util/is_all.hpp
@@ -1,6 +1,6 @@
-#ifndef COFFEE_MILL_VARIADIC_TMEPLATE
-#define COFFEE_MILL_VARIADIC_TMEPLATE
-#include "meta.hpp"
+#ifndef COFFEE_MILL_IS_ALL
+#define COFFEE_MILL_IS_ALL
+#include <mill/util/meta.hpp>
 
 namespace mill
 {
@@ -8,27 +8,19 @@ namespace mill
 template<template<typename...> class, typename ...>
 struct is_all;
 
-template<template<typename> class condT, typename frontT, typename ... Ts>
-struct is_all<condT, frontT, Ts...>
-    : public _and_<condT<frontT>, is_all<condT, Ts...>>::type
-{};
+// is_all<Unary, Xs...>
+template<template<typename> class F, typename X, typename ... Xs>
+struct is_all<F, X, Xs...> :
+conjunction<F<X>, typename is_all<F, Xs...>::type>::type {};
+template<template<typename> class F, typename X>
+struct is_all<F, X> : F<X>::type{};
 
-template<template<typename> class condT, typename lastT>
-struct is_all<condT, lastT>
-    : public condT<lastT>::type
-{};
-
-template<template<typename, typename> class condT, typename refT,
-         typename frontT, typename ... Ts>
-struct is_all<condT, refT, frontT, Ts...>
-    : public _and_<condT<refT, frontT>, is_all<condT, refT, Ts...>>::type
-{};
-
-template<template<typename, typename> class condT, typename refT, typename lastT>
-struct is_all<condT, refT, lastT>
-    : public condT<refT, lastT>::type
-{};
+// is_all<Binary, ARG1, Xs...>
+template<template<typename, typename> class F, typename X, typename Y, typename ... Ys>
+struct is_all<F, X, Y, Ys...> :
+conjunction<F<X, Y>, typename is_all<F, X, Ys...>::type>::type{};
+template<template<typename, typename> class F, typename X, typename Y>
+struct is_all<F, X, Y> : F<X, Y>::type {};
 
 } // mill
-
-#endif /* COFFEE_MILL_VARIADIC_TMEPLATE */
+#endif /* COFFEE_MILL_IS_ALL */
--- mill/util/meta.hpp
+++ mill/util/meta.hpp
@@ -1,36 +1,61 @@
 #ifndef COFFEE_MILL_META
 #define COFFEE_MILL_META
 #include <type_traits>
+#include <mill/util/has_xxx.hpp>
 
 namespace mill
 {
-template<typename ...>
-struct _or_;
-template<>
-struct _or_<> : public std::false_type {};
-template<typename T>
-struct _or_<T> : public T {};
-template<typename T1, typename T2>
-struct _or_<T1, T2> : public std::conditional<T1::value, T1, T2>::type {};
-template<typename T1, typename T2, typename T3, typename ... Ts>
-struct _or_<T1, T2, T3, Ts...> 
-: public std::conditional<T1::value, T1, _or_<T2, T3, Ts...>>::type {};
 
-template<typename ...>
-struct _and_;
-template<>
-struct _and_<> : public std::true_type {};
+// and
+template<typename ... Ts>
+struct conjunction : public std::true_type {};
 template<typename T>
-struct _and_<T> : public T {};
-template<typename T1, typename T2>
-struct _and_<T1, T2> : public std::conditional<T1::value, T2, T1>::type {};
-template<typename T1, typename T2, typename T3, typename ... Ts>
-struct _and_<T1, T2, T3, Ts...> 
-: public std::conditional<T1::value, _and_<T2, T3, Ts...>, T1>::type {};
+struct conjunction<T> : public T
+{
+    static_assert(has_v_value<T>::value, "conjunction requires parameter T"
+        " to have static bool value named as `value`.");
+    static_assert(has_t_type<T>::value,  "conjunction requires parameter T"
+        " to have member type named as `type`.");
+    static_assert(has_t_type<T>::value,  "conjunction requires parameter T"
+        " to have member type named as `value_type`.");
+};
+template<typename T, typename ... Ts>
+struct conjunction<T, Ts...> : public std::conditional<bool(T::value),
+    conjunction<Ts...>, T>{
+    static_assert(has_v_value<T>::value, "conjunction requires parameter T"
+        " to have static bool value named as `value`.");
+    static_assert(has_t_type<T>::value,  "conjunction requires parameter T"
+        " to have member type named as `type`.");
+    static_assert(has_t_type<T>::value,  "conjunction requires parameter T"
+        " to have member type named as `value_type`.");
+};
+
+// or
+template<typename ... Ts>
+struct disjunction : public std::false_type {};
+template<typename T>
+struct disjunction<T> : public T
+{
+    static_assert(has_v_value<T>::value, "disjunction requires parameter T"
+        " to have static bool value named as `value`.");
+    static_assert(has_t_type<T>::value,  "disjunction requires parameter T"
+        " to have member type named as `type`.");
+    static_assert(has_t_type<T>::value,  "disjunction requires parameter T"
+        " to have member type named as `value_type`.");
+};
+template<typename T, typename ... Ts>
+struct disjunction<T, Ts...> : public std::conditional<bool(T::value),
+    T, disjunction<Ts...>>{
+    static_assert(has_v_value<T>::value, "disjunction requires parameter T"
+        " to have static bool value named as `value`.");
+    static_assert(has_t_type<T>::value,  "disjunction requires parameter T"
+        " to have member type named as `type`.");
+    static_assert(has_t_type<T>::value,  "disjunction requires parameter T"
+        " to have member type named as `value_type`.");
+};
 
 template<typename T>
-struct _not_ : public std::integral_constant<bool, !T::value>{};
-
+struct negation : public std::integral_constant<bool, !bool(T::value)> {};
 
 }//mill
 #endif// COFFEE_MILL_META
