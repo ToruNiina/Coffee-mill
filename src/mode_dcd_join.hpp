25037eb93343a6933282c44051dcbdaa85a73f60
ToruNiina <niina.toru.68u@gmail.com>
2017-12-21 17:03:47 +0900

    update mode_dcd_join

--- src/mode_dcd_join.hpp
+++ src/mode_dcd_join.hpp
@@ -1,96 +1,165 @@
 #ifndef COFFEE_MILL_DCD_JOIN
 #define COFFEE_MILL_DCD_JOIN
-#include "data/DCDReader.hpp"
-#include "data/DCDWriter.hpp"
-#include "toml/toml.hpp"
+#include <mill/data/DCDReader.hpp>
+#include <mill/data/DCDWriter.hpp>
+#include <fstream>
+#include <toml/toml.hpp>
 
 namespace mill
 {
 
-template<typename vectorT>
-int mill_dcd_join(int argument_c, char** argument_v)
+inline const char* dcd_join_usage() noexcept
 {
-    const std::string fname(argument_v[0]);
+    return "usage: mill dcd join [parameters...]\n"
+           "    $ mill dcd join traj.dcd traj1.dcd traj2.dcd\n"
+           "    : concatenate traj.dcd, traj1.dcd and traj2.dcd into traj1_joined.dcd\n"
+           "    $ mill dcd join input.toml\n"
+           "    : specify path/to/dcdfile and output filename\n\n"
+           "    # example.toml:\n"
+           "    inputs = [\"data/traj1.dcd\", \"data/traj2.dcd\"]\n"
+           "    output = \"data/traj.dcd\"\n"
+           "    include_initial = true # by default, it's true\n"
+           "    # if include_initial is false, this command removes initial snapshot from successive files.\n"
+           "    # it is for the case if the last snapshot of a file and the first snapshot of the next file is same.\n";
+}
+
+//! argv = {"join", {args...}}
+template<typename vectorT>
+int mode_dcd_join(int argument_c, const char** argument_v)
+{
+    if(argument_c == 1)
+    {
+        std::cerr << "error: mill dcd-mode: too few arguments\n";
+        std::cerr << dcd_join_usage() << std::endl;
+        return 1;
+    }
+
+    const std::string fname(argument_v[1]);
+    if(fname == "help")
+    {
+        std::cerr << dcd_join_usage() << std::endl;
+        return 0;
+    }
+    if(fname.size() < 5)
+    {
+        std::cerr << "error: mill dcd join: invalid argument : "
+                  << fname << '\n';
+        std::cerr << dcd_join_usage() << std::endl;
+        return 1;
+    }
+
     if(fname.substr(fname.size() - 4, 4) == ".dcd")
-    {// join all the dcd files...
+    {
+        //! argv = {"join", {"1.dcd", "2.dcd", ...}}
         DCDReader<vectorT> reader;
         auto dcddata = reader.read_header(fname);
+
         // check total steps
-        for(int i=1; i<argument_c; ++i)
+        for(int i=2; i<argument_c; ++i)
         {
             const std::string filename(argument_v[i]);
-            const auto tmpdata = reader.read_header(filename);
-            if(tmpdata.nparticle() != dcddata.nparticle())
-                throw std::invalid_argument("different structure");
-            dcddata.nset() += tmpdata.nset();
+            const auto tmphdr = reader.read_header(filename);
+
+            if(tmphdr.nparticle() != dcddata.nparticle())
+            {
+                std::cerr << "error: mill dcd join: "
+                          << "dcd files have different structures!\n";
+                std::cerr << "file " << fname << " has " << dcddata.nparticle()
+                          << ", but file " << filename << " has "
+                          << tmphdr.nparticle() << std::endl;
+                return 1;
+            }
+            dcddata.nset() += tmphdr.nset();
         }
 
-        // write all the dcd files
-        const std::string outname = fname.substr(0, fname.size() - 4) +
-                                    "_joined.dcd";
+        const std::string outname =
+            fname.substr(0, fname.size() - 4) + "_joined.dcd";
+
         std::ofstream ofs(outname);
         if(not ofs.good())
-            throw std::runtime_error("file open error: " + outname);
+        {
+            std::cerr << "error: mill dcd join: output file open error: "
+                      << outname << std::endl;
+            return 1;
+        }
 
         DCDWriter<vectorT> writer;
         writer.write_header(ofs, dcddata.header());
         for(int i=0; i<argument_c; ++i)
         {
             const std::string filename(argument_v[i]);
-            std::ifstream ifs(filename);
-            const auto tmpdata = reader.read_header(ifs);
-            writer.write_snapshot(ofs, reader.read_snapshot(ifs, dcddata.header()));
+            const auto tmpdata = reader.read(filename);
+            for(const auto& ss : tmpdata)
+            {
+                writer.write_snapshot(ofs, ss);
+            }
         }
         ofs.close();
+
         return 0;
     }
-    else if(fname.substr(fname.size() - 4, 4) == "toml")
-    {// toml file exists. read detail setting.
-        std::ifstream tomlfile(fname);
-        if(not tomlfile.good())
-            throw std::invalid_argument("file open error: " + fname);
-
-        const toml::Data tomldata = toml::parse(tomlfile);
-        const std::vector<std::string> inputs =
-            toml::get<toml::Array<toml::String>>(tomldata.at("inputs"));
-        const std::string outname = toml::get<toml::String>(tomldata.at("output"));
-        bool redundant = false;
-        try{redundant = toml::get<toml::Boolean>(tomldata.at("redundancy"));}
-        catch(std::exception& excpt){/*do nothing*/}
+    else if(fname.substr(fname.size() - 5, 5) == "toml")
+    {
+        const auto tomldata = toml::parse(fname);
+        const auto inputs =
+            toml::get<std::vector<std::string>>(tomldata.at("inputs"));
+        const auto output =
+            toml::get<std::string>(tomldata.at("output"));
+        const bool include_initial =
+            toml::get_or(tomldata, "include_initial", true);
 
         DCDReader<vectorT> reader;
         auto dcddata = reader.read_header(inputs.front());
+
         // check total steps
         for(std::size_t i=1; i<inputs.size(); ++i)
         {
-            const std::string filename(inputs[i]);
+            const auto& filename = inputs[i];
             const auto tmpdata = reader.read_header(filename);
             if(tmpdata.nparticle() != dcddata.nparticle())
-                throw std::invalid_argument("different structure");
+            {
+                std::cerr << "error: mill dcd join: "
+                          << "dcd files have different structures!\n";
+                std::cerr << "file " << inputs.front() << " has "
+                          << dcddata.nparticle()
+                          << ", but file " << filename << " has "
+                          << tmpdata.nparticle() << std::endl;
+                return 1;
+            }
             dcddata.nset() += tmpdata.nset();
         }
 
-        std::ofstream ofs(outname);
+        std::ofstream ofs(output);
         if(not ofs.good())
-            throw std::runtime_error("file open error: " + outname);
+        {
+            std::cerr << "error: mill dcd join: output file open error: "
+                      << output << std::endl;
+            return 1;
+        }
 
         DCDWriter<vectorT> writer;
         writer.write_header(ofs, dcddata.header());
-        for(int i=0; i<argument_c; ++i)
+        for(const auto& filename : inputs)
         {
-            const std::string filename(argument_v[i]);
-            std::ifstream ifs(filename);
-            const auto tmpdata = reader.read_header(ifs);
-            if(not redundant) auto tmp = reader.read_snapshot(ifs, tmpdata.header());
-
-            writer.write_snapshot(ofs, reader.read_snapshot(ifs, tmpdata.header()));
+            const auto tmpdata = reader.read(filename);
+            if(include_initial)
+            {
+                writer.write_snapshot(ofs, tmpdata.front());
+            }
+            for(auto iter = tmpdata.cbegin()+1; iter != tmpdata.cend(); ++iter)
+            {
+                writer.write_snapshot(ofs, *iter);
+            }
         }
         ofs.close();
         return 0;
     }
     else
     {
-        throw std::invalid_argument("unknown file in argument: " + fname);
+        std::cerr << "error: mill dcd join: invalid argument : "
+                  << fname << '\n';
+        std::cerr << dcd_join_usage() << std::endl;
+        return 1;
     }
 }
 
