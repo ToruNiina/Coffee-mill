bbe397dd6630f487dd46cc66dcca298a54184140
ToruNiina <niina.toru.68u@gmail.com>
2017-12-21 19:59:40 +0900

    update pdb mode

--- src/mode_pdb.hpp
+++ src/mode_pdb.hpp
@@ -2,24 +2,33 @@
 #define COFFEE_MILL_PDB_MODE
 #include <mill/data/PDBReader.hpp>
 #include <mill/data/PDBWriter.hpp>
-#include "mill_pdb_seq.hpp"
-#include "mill_pdb_mutate.hpp"
+#include <src/mode_pdb_help.hpp>
+#include <src/mode_pdb_seq.hpp>
+#include <src/mode_pdb_mutate.hpp>
+#include <iostream>
 
 namespace mill
 {
 
+// argv := arrayof{ "pdb", "command-name", {rests...} }
 template<typename vectorT>
 int mode_pdb(int argument_c, char **argument_v)
 {
-    if(argument_c < 2) throw std::invalid_argument("too few commands");
+    if(argument_c < 2)
+    {
+        std::cerr << "error: mill dcd-mode: too few arguments\n";
+        mode_dcd_help(--argument_c, ++argument_v); // {}
+        return 1;
+    }
+
     const std::string command(argument_v[1]);
     if(command == "seq")
     {
-        return mill_pdb_seq<vectorT>(--argument_c, ++argument_v);
+        return mode_pdb_seq<vectorT>(--argument_c, ++argument_v);
     }
     else if(command == "mutate")
     {
-        return mill_pdb_mutate<vectorT>(--argument_c, ++argument_v);
+        return mode_pdb_mutate<vectorT>(--argument_c, ++argument_v);
     }
     else
     {
---
+++ src/mode_pdb_help.hpp
@@ -0,0 +1,55 @@
+#ifndef COFFEE_MILL_PDB_HELP_MODE
+#define COFFEE_MILL_PDB_HELP_MODE
+#include <iostream>
+#include <mill/math/Vector.hpp>
+#include <src/mode_pdb_seq.hpp>
+#include <src/mode_pdb_mutate.hpp>
+
+namespace mill
+{
+
+inline const char* mode_pdb_help_usage() noexcept
+{
+    return "usage: mill pdb [command] [parameters...]\n\n"
+           "    avaiable commands\n"
+           "    - seq\n"
+           "        print a sequence of each chain\n"
+           "    - mutate\n"
+           "        mutate coarse-grained chain by renaming residues\n"
+           "    - help\n"
+           "        print detailed explanation of each command\n";
+}
+
+//! this function forwards the arguments to different modes.
+// argv := { "help" or "pdb", {args...} }
+inline int mode_pdb_help(int argument_c, const char **argument_v)
+{
+    if(argument_c < 2)
+    {
+        std::cerr << mode_pdb_help_usage() << std::endl;
+        return 0;
+    }
+
+    const std::string command(argument_v[1]);
+
+    if(command == "seq")
+    {
+        const char *cmds[2] = {"seq", "help"};
+        return mode_pdb_seq<mill::Vector<double, 3>>(2, cmds);
+    }
+    else if(command == "mutate")
+    {
+        const char *cmds[2] = {"mutate", "help"};
+        return mode_pdb_mutate<mill::Vector<double, 3>>(2, cmds);
+    }
+    else
+    {
+        std::cerr << "error: mill pdb help: unknown command : "
+                  << command << "\n\n";
+        std::cerr << mode_pdb_help_usage() << std::endl;
+        return 1;
+    }
+}
+
+} // mill
+#endif// COFFEE_MILL_pdb_HELP_MODE
