688c278ef3c8efb4d55effacc8f9543fa134b77f
ToruNiina <niina.toru.68u@gmail.com>
2017-12-21 20:21:59 +0900

    add pdb mode

--- src/mode_help.hpp
+++ src/mode_help.hpp
@@ -2,7 +2,7 @@
 #define COFFEE_MILL_HELP_MODE
 #include <iostream>
 #include <src/mode_dcd_help.hpp>
-// #include <src/mode_pdb.hpp>
+#include <src/mode_pdb_help.hpp>
 // #include <src/mode_ninfo.hpp>
 
 namespace mill
@@ -30,16 +30,15 @@ inline int mode_help(int argument_c, const char **argument_v)
 
     const std::string command(argument_v[1]);
 
-//     if(command == "pdb")
-//     {
-//         return mode_pdb_help(--argument_c, ++argument_v);
-//     }
+    if(command == "pdb")
+    {
+        return mode_pdb_help(--argument_c, ++argument_v);
+    }
 //     else if(command == "ninfo")
 //     {
 //         return mode_ninfo_help(--argument_c, ++argument_v);
 //     }
-//     else
-        if(command == "dcd")
+    else if(command == "dcd")
     {
         return mode_dcd_help(--argument_c, ++argument_v);
     }
