a614a4cd0e6c1bec627928e73af0171b10fda509
ToruNiina <niina.toru.68u@gmail.com>
2017-12-21 15:48:45 +0900

    make usage string and enable to pass argument to help of each mode

--- src/mode_help.hpp
+++ src/mode_help.hpp
@@ -8,48 +8,46 @@
 namespace mill
 {
 
-inline void print_main_help()
+inline const char* main_usage()
 {
-    std::cerr << "Usage: mill [mode] [parameters...]\n\n";
-    std::cerr << "list of modes\n";
-    std::cerr << "\tpdb\n";
-    std::cerr << "\tdcd\n";
-    std::cerr << "\tninfo\n";
-    std::cerr << "\thelp\n";
-    std::cerr << std::flush;
-    return;
+    return "Usage: mill [mode] [parameters...]\n\n"
+           "list of modes\n"
+           "\t- pdb\n"
+           "\t- dcd\n"
+           "\t- ninfo\n"
+           "\t- help\n";
 }
 
 //! this function forwards the arguments to different modes.
 // argv := { "help", {args...} }
-inline int mode_help(int argument_c, char **argument_v)
+inline int mode_help(int argument_c, const char **argument_v)
 {
     if(argument_c < 2)
     {
-        print_main_help();
+        std::cerr << main_usage() << std::endl;
         return 0;
     }
 
     const std::string command(argument_v[1]);
+
     if(command == "pdb")
     {
-        return mode_pdb(2, argument_v);
+        return mode_pdb_help(--argument_c, ++argument_v);
     }
     else if(command == "ninfo")
     {
-        return mode_ninfo(2, argument_v);
+        return mode_ninfo_help(--argument_c, ++argument_v);
     }
     else if(command == "dcd")
     {
-        return mode_dcd(2, argument_v);
+        return mode_dcd_help(--argument_c, ++argument_v);
     }
     else
     {
-        std::cerr << "mill help: unknown command \"" << command << "\"\n\n";
-        print_main_help();
+        std::cerr << "mill help: unknown command : " << command << "\n\n";
+        std::cerr << main_usage() << std::endl;
         return 1;
     }
-    return 0;
 }
 
 } // mill
