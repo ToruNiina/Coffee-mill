f5fc1aa2ec068035951d4519d1431e22c7947723
ToruNiina <niina.toru.68u@gmail.com>
2019-07-22 23:35:27 +0900

    feat: add all-in-one header for math funcs

---
+++ mill/math.hpp
@@ -0,0 +1,10 @@
+#ifndef COFFEE_MILL_INCLUDE_MATH_HPP
+#define COFFEE_MILL_INCLUDE_MATH_HPP
+
+#include "math/BestFitStructure.hpp"
+#include "math/EigenSolver.hpp"
+#include "math/Matrix.hpp"
+#include "math/RMSDCalculator.hpp"
+#include "math/Vector.hpp"
+
+#endif// COFFEE_MILL_INCLUDE_MATH_HPP
