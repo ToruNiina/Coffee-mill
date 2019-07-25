836ee58e84d7f51f6a8b3d2e86a240757387ef83
ToruNiina <niina.toru.68u@gmail.com>
2019-07-25 22:29:39 +0900

    :sparkles: extend template argument of utility funcs

--- mill/util/string.hpp
+++ mill/util/string.hpp
@@ -12,22 +12,27 @@ inline bool is_whitespace(charT c)
     return (c == ' ') || (c == '\t');
 }
 
-template<typename charT>
-std::basic_string<charT>
-remove_indent(std::basic_string<charT> const& str)
+template<typename charT, typename Traits, typename Alloc>
+std::basic_string<charT, Traits, Alloc>
+remove_indent(const std::basic_string<charT, Traits, Alloc>& str)
 {
     auto iter = str.cbegin();
     while(is_whitespace(*iter) && (iter != str.cend())){++iter;}
     return std::basic_string<charT>(iter, str.cend());
 }
 
-template<typename charT>
-std::basic_string<charT>
-remove_all(const charT c, const std::basic_string<charT>& str)
+template<typename charT, typename Traits, typename Alloc>
+std::basic_string<charT, Traits, Alloc>
+remove_all(const charT c, const std::basic_string<charT, Traits, Alloc>& str)
 {
     std::basic_string<charT> retval;
     for(auto iter = str.cbegin(); iter != str.cend(); ++iter)
-        if(*iter != c) retval += *iter;
+    {
+        if(*iter != c)
+        {
+            retval += *iter;
+        }
+    }
     return retval;
 }
 
