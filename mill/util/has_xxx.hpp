f340d254a1111fd42f54208456c55305f3b85611
ToruNiina <niina.toru.68u@gmail.com>
2019-07-25 23:05:35 +0900

    :art: correct indentation and macro backslash

--- mill/util/has_xxx.hpp
+++ mill/util/has_xxx.hpp
@@ -3,29 +3,29 @@
 #include <type_traits>
 
 #ifndef COFFEE_MILL_HAS_MEMBER_TYPE
-#define COFFEE_MILL_HAS_MEMBER_TYPE( name )\
-namespace detail {\
-struct has_ ## name ## _t_impl {\
-    template<class T> static std::true_type  check(typename T::name*);\
-    template<class T> static std::false_type check(...);\
-};\
-}\
-template<class T> struct has_t_ ## name :\
-    decltype(detail::has_ ## name ## _t_impl::check<T>(nullptr)) {};\
-/**/
+#define COFFEE_MILL_HAS_MEMBER_TYPE( name )                               \
+    namespace detail {                                                    \
+    struct has_ ## name ## _t_impl {                                      \
+        template<class T> static std::true_type  check(typename T::name*);\
+        template<class T> static std::false_type check(...);              \
+    };                                                                    \
+    }                                                                     \
+    template<class T> struct has_t_ ## name :                             \
+        decltype(detail::has_ ## name ## _t_impl::check<T>(nullptr)) {};  \
+    /**/
 #endif // HAS_MEMBER_TYPE
 
 #ifndef COFFEE_MILL_HAS_STATIC_VALUE
-#define COFFEE_MILL_HAS_STATIC_VALUE( name )\
-namespace detail {\
-struct has_ ## name ## _v_impl {\
-    template<class T> static std::true_type  check(decltype(&T::name)*);\
-    template<class T> static std::false_type check(...);\
-};\
-}\
-template<class T> struct has_v_ ## name :\
-    decltype(detail::has_ ## name ## _v_impl::check<T>(nullptr)) {};\
-/**/
+#define COFFEE_MILL_HAS_STATIC_VALUE( name )                                 \
+    namespace detail {                                                       \
+    struct has_ ## name ## _v_impl {                                         \
+        template<class T> static std::true_type  check(decltype(&T::name)*); \
+        template<class T> static std::false_type check(...);                 \
+    };                                                                       \
+    }                                                                        \
+    template<class T> struct has_v_ ## name :                                \
+        decltype(detail::has_ ## name ## _v_impl::check<T>(nullptr)) {};     \
+    /**/
 #endif // HAS_STATIC_VALUE
 
 namespace mill
@@ -34,9 +34,6 @@ namespace mill
 COFFEE_MILL_HAS_MEMBER_TYPE(value_type) // mill::has_t_value_type
 COFFEE_MILL_HAS_MEMBER_TYPE(type)       // mill::has_t_type
 COFFEE_MILL_HAS_STATIC_VALUE(value)     // mill::has_v_value
-
-
-// in-line tests
 
 static_assert(has_t_value_type<std::true_type>::value, "mill::has_t_value_type<true_type> failed!");
 static_assert(has_t_type<std::true_type>::value,       "mill::has_t_type<true_type> failed!");
@@ -48,5 +45,5 @@ static_assert(has_v_value<int>::value == false,        "mill::has_v_value<int> f
 } // mill
 
 #undef COFFEE_MILL_HAS_MEMBER_TYPE
-
+#undef COFFEE_MILL_HAS_STATIC_VALUE
 #endif// COFFEE_MILL_HAS_XXX
