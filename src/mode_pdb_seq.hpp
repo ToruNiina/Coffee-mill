dd6a20ef31d73f7a851dc12db5c0b3fb78ccfa86
ToruNiina <niina.toru.68u@gmail.com>
2019-07-26 23:41:16 +0900

    :recycle: use utility class for amino acid code

--- src/mode_pdb_seq.hpp
+++ src/mode_pdb_seq.hpp
@@ -2,6 +2,7 @@
 #define COFFEE_MILL_PDB_SEQ
 #include <mill/pdb/PDBChain.hpp>
 #include <mill/util/string.hpp>
+#include <mill/util/amino_acid_code.hpp>
 #include <algorithm>
 #include <map>
 
@@ -14,60 +15,6 @@ inline const char* pdb_seq_usage() noexcept
            "    $ mill pdb seq model.pdb\n"
            "      print sequences for each chain\n";
 }
-
-struct amino_acid_code
-{
-    char operator()(std::string const& s) const {return codes.at(s);}
-    std::string const& operator()(char s) const
-    {
-        return std::find_if(codes.cbegin(), codes.cend(),
-            [=](const std::pair<std::string, char>& p){return p.second==s;}
-            )->first;
-    }
-
-    char               at(const std::string& s) const {return codes.at(s);}
-    std::string const& at(const char s) const
-    {
-        const auto found = std::find_if(codes.cbegin(), codes.cend(),
-            [=](const std::pair<std::string, char>& p){return p.second==s;}
-            );
-        if(found != codes.cend())
-        {
-            return found->first;
-        }
-        else
-        {
-            throw std::out_of_range("invalid amino acid code");
-        }
-    }
-
-    std::map<std::string, char> codes = {
-        {"ALA", 'A'},
-        {"ASX", 'B'},
-        {"CYS", 'C'},
-        {"ASP", 'D'},
-        {"GLU", 'E'},
-        {"PHE", 'F'},
-        {"GLY", 'G'},
-        {"HIS", 'H'},
-        {"ILE", 'I'},
-        {"LYS", 'K'},
-        {"LEU", 'L'},
-        {"MET", 'M'},
-        {"ASN", 'N'},
-        {"PRO", 'P'},
-        {"GLN", 'Q'},
-        {"ARG", 'R'},
-        {"SER", 'S'},
-        {"THR", 'T'},
-        {"SEC", 'U'},
-        {"VAL", 'V'},
-        {"TRP", 'W'},
-        {"XAA", 'X'},
-        {"TYR", 'Y'},
-        {"GLX", 'Z'}
-    };
-};
 
 template<typename Iterator>
 struct residue_name_iterator
