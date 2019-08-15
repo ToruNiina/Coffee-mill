05bb0a9467697c9cc35b45c4718d8f03540d242e
ToruNiina <niina.toru.68u@gmail.com>
2019-08-15 23:43:58 +0900

    :white_check_mark: add test for utility functions

---
+++ tests/util/test_string.cpp
@@ -0,0 +1,43 @@
+#define BOOST_TEST_MODULE "test_util_string"
+
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_NO_LIB
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include "util/string.hpp"
+
+BOOST_AUTO_TEST_CASE(remove_indent)
+{
+    {
+        const std::string str(" test");
+        const std::string ret = mill::remove_indent(str);
+        BOOST_TEST(ret == "test");
+    }
+    {
+        const std::string str("\ttest");
+        const std::string ret = mill::remove_indent(str);
+        BOOST_TEST(ret == "test");
+    }
+    {
+        const std::string str("  test");
+        const std::string ret = mill::remove_indent(str);
+        BOOST_TEST(ret == "test");
+    }
+    {
+        const std::string str("\t\ttest");
+        const std::string ret = mill::remove_indent(str);
+        BOOST_TEST(ret == "test");
+    }
+}
+
+BOOST_AUTO_TEST_CASE(remove_all)
+{
+    {
+        const std::string str("test");
+        const std::string ret = mill::remove_all('t', str);
+        BOOST_TEST(ret == "es");
+    }
+}
