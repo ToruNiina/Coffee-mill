2f160e40a3a699a3c166c4d05940af33346d3b80
ToruNiina <niina.toru.68u@gmail.com>
2019-08-16 20:47:27 +0900

    :pencil2: add missing parentheses

--- mill/util/logger.hpp
+++ mill/util/logger.hpp
@@ -30,33 +30,37 @@ struct Logger
     template<typename ... Ts>
     void error(Ts&& ... args) const
     {
-        if(not this->is_activated(log_level::error) {return;}
+        if(not this->is_activated(log_level::error)) {return;}
         std::cerr << "\x1b[31mError:\x1b[0m ";
         (std::cerr << ... << args);
+        std::cerr << std::flush;
         return;
     }
     template<typename ... Ts>
     void warn(Ts&& ... args) const
     {
-        if(not this->is_activated(log_level::warn) {return;}
+        if(not this->is_activated(log_level::warn)) {return;}
         std::cerr << "\x1b[33mWarn:\x1b[0m ";
         (std::cerr << ... << args);
+        std::cerr << std::flush;
         return;
     }
     template<typename ... Ts>
     void info(Ts&& ... args) const
     {
-        if(not this->is_activated(log_level::info) {return;}
+        if(not this->is_activated(log_level::info)) {return;}
         std::cerr << "\x1b[32mInfo:\x1b[0m ";
         (std::cerr << ... << args);
+        std::cerr << std::flush;
         return;
     }
     template<typename ... Ts>
     void debug(Ts&& ... args) const
     {
-        if(not this->is_activated(log_level::debug) {return;}
+        if(not this->is_activated(log_level::debug)) {return;}
         std::cerr << "\x1b[34mDebug:\x1b[0m ";
         (std::cerr << ... << args);
+        std::cerr << std::flush;
         return;
     }
 
@@ -90,6 +94,7 @@ void log(log_level level, Ts&& ... args)
         {
             std::cerr << "\x1b[30;1mUnknown Level:\x1b[0m ";
             (std::cerr << ... << args);
+            std::cerr << std::flush;
             return;
         }
     }
