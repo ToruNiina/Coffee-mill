e00dbc45deaf01c6809cd7dfcac9d0f3b624da12
ToruNiina <niina.toru.68u@gmail.com>
2020-01-18 22:58:03 +0900

    :recycle: do not require to pass log_level

--- mill/util/logger.hpp
+++ mill/util/logger.hpp
@@ -6,8 +6,9 @@
 
 namespace mill
 {
-
-enum class log_level : std::uint8_t
+namespace log
+{
+enum class level : std::uint8_t
 {
     error,
     warn ,
@@ -27,79 +28,48 @@ struct Logger
     void activate  (const log_level lv) {filter_[lv] = true;}
     void deactivate(const log_level lv) {filter_[lv] = false;}
 
-    template<typename ... Ts>
-    void error(Ts&& ... args) const
-    {
-        if(not this->is_activated(log_level::error)) {return;}
-        std::cerr << "\x1b[31mError:\x1b[0m ";
-        (std::cerr << ... << args);
-        std::cerr << std::flush;
-        return;
-    }
-    template<typename ... Ts>
-    void warn(Ts&& ... args) const
-    {
-        if(not this->is_activated(log_level::warn)) {return;}
-        std::cerr << "\x1b[33mWarn :\x1b[0m ";
-        (std::cerr << ... << args);
-        std::cerr << std::flush;
-        return;
-    }
-    template<typename ... Ts>
-    void info(Ts&& ... args) const
-    {
-        if(not this->is_activated(log_level::info)) {return;}
-        std::cerr << "\x1b[32mInfo :\x1b[0m ";
-        (std::cerr << ... << args);
-        std::cerr << std::flush;
-        return;
-    }
-    template<typename ... Ts>
-    void debug(Ts&& ... args) const
-    {
-        if(not this->is_activated(log_level::debug)) {return;}
-        std::cerr << "\x1b[34mDebug:\x1b[0m ";
-        (std::cerr << ... << args);
-        std::cerr << std::flush;
-        return;
-    }
-
   private:
     std::map<log_level, bool> filter_;
 };
 inline Logger logger;
 
 template<typename ... Ts>
-void log(log_level level, Ts&& ... args)
+void error(Ts&& ... args) const
 {
-    switch(level)
-    {
-        case log_level::error:
-        {
-            return logger.error(std::forward<Ts>(args)...);
-        }
-        case log_level::warn:
-        {
-            return logger.warn (std::forward<Ts>(args)...);
-        }
-        case log_level::info:
-        {
-            return logger.info (std::forward<Ts>(args)...);
-        }
-        case log_level::debug:
-        {
-            return logger.debug(std::forward<Ts>(args)...);
-        }
-        default:
-        {
-            std::cerr << "\x1b[30;1mUnknown Level:\x1b[0m ";
-            (std::cerr << ... << args);
-            std::cerr << std::flush;
-            return;
-        }
-    }
+    if(not this->is_activated(log_level::error)) {return;}
+    std::cerr << "\x1b[31mError:\x1b[0m ";
+    (std::cerr << ... << args);
+    std::cerr << std::flush;
+    return;
+}
+template<typename ... Ts>
+void warn(Ts&& ... args) const
+{
+    if(not this->is_activated(log_level::warn)) {return;}
+    std::cerr << "\x1b[33mWarn :\x1b[0m ";
+    (std::cerr << ... << args);
+    std::cerr << std::flush;
+    return;
+}
+template<typename ... Ts>
+void info(Ts&& ... args) const
+{
+    if(not this->is_activated(log_level::info)) {return;}
+    std::cerr << "\x1b[32mInfo :\x1b[0m ";
+    (std::cerr << ... << args);
+    std::cerr << std::flush;
+    return;
+}
+template<typename ... Ts>
+void debug(Ts&& ... args) const
+{
+    if(not this->is_activated(log_level::debug)) {return;}
+    std::cerr << "\x1b[34mDebug:\x1b[0m ";
+    (std::cerr << ... << args);
+    std::cerr << std::flush;
     return;
 }
 
+} // log
 } // mill
 #endif// COFFEE_MILL_UTIL_LOGGER_HPP
