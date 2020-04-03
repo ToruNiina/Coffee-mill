0c4aecf49269cffbf8b6f7bde2e82df131a9f0eb
ToruNiina <niina.toru.68u@gmail.com>
2020-04-04 00:49:38 +0900

    :art: add blocks explicitly

--- mill/math/RMSDCalculator.hpp
+++ mill/math/RMSDCalculator.hpp
@@ -11,13 +11,16 @@ realT rmsd(const std::vector<Vector<realT, N>>& lhs,
 {
     const std::size_t s = lhs.size();
     if(s != rhs.size())
+    {
         throw std::invalid_argument("rmsd: different size");
+    }
 
     realT sigma = 0.;
-    for(std::size_t i=0; i<s; ++i) sigma += length_sq(lhs[i] - rhs[i]);
-    const realT invN = 1. / static_cast<realT>(s);
-
-    return std::sqrt(sigma * invN);
+    for(std::size_t i=0; i<s; ++i)
+    {
+        sigma += length_sq(lhs[i] - rhs[i]);
+    }
+    return std::sqrt(sigma / static_cast<realT>(s));
 }
 
 }// mill
