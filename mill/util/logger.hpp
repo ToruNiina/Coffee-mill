e5122ec7c296ff433e4834e0b7fba2373de0f7b7
ToruNiina <niina.toru.68u@gmail.com>
2020-06-02 02:04:22 +0900

    :lipstick: suppress ANSI color codes if cerr is not a tty

--- mill/util/logger.hpp
+++ mill/util/logger.hpp
@@ -1,5 +1,6 @@
 #ifndef COFFEE_MILL_UTIL_LOGGER_HPP
 #define COFFEE_MILL_UTIL_LOGGER_HPP
+#include <mill/util/isatty.hpp>
 #include <map>
 #include <iostream>
 #include <cstdint>
@@ -37,36 +38,44 @@ template<typename ... Ts>
 void error(Ts&& ... args)
 {
     if(not logger.is_activated(level::error)) {return;}
-    std::cerr << "\x1b[31mError:\x1b[0m ";
+
+    if(isatty(std::cerr)) {std::cerr << "\x1b[31mError:\x1b[0m ";}
+    else                  {std::cerr <<         "Error: ";}
     (std::cerr << ... << args);
-    std::cerr << std::endl;
+    std::cerr << std::endl; // logs should be flashed every time
     return;
 }
 template<typename ... Ts>
 void warn(Ts&& ... args)
 {
     if(not logger.is_activated(level::warn)) {return;}
-    std::cerr << "\x1b[33mWarn :\x1b[0m ";
+
+    if(isatty(std::cerr)) {std::cerr << "\x1b[33mWarn :\x1b[0m ";}
+    else                  {std::cerr <<         "Warn : ";}
     (std::cerr << ... << args);
-    std::cerr << std::endl;
+    std::cerr << std::endl; // logs should be flashed every time
     return;
 }
 template<typename ... Ts>
 void info(Ts&& ... args)
 {
     if(not logger.is_activated(level::info)) {return;}
-    std::cerr << "\x1b[32mInfo :\x1b[0m ";
+
+    if(isatty(std::cerr)) {std::cerr << "\x1b[32mInfo :\x1b[0m ";}
+    else                  {std::cerr <<         "Info : ";}
     (std::cerr << ... << args);
-    std::cerr << std::endl;
+    std::cerr << std::endl; // logs should be flashed every time
     return;
 }
 template<typename ... Ts>
 void debug(Ts&& ... args)
 {
     if(not logger.is_activated(level::debug)) {return;}
-    std::cerr << "\x1b[34mDebug:\x1b[0m ";
+
+    if(isatty(std::cerr)) {std::cerr << "\x1b[34mDebug:\x1b[0m ";}
+    else                  {std::cerr <<         "Debug: ";}
     (std::cerr << ... << args);
-    std::cerr << std::endl;
+    std::cerr << std::endl; // logs should be flashed every time
     return;
 }
 
