407506655768624c318ba10e0b642dfae4952a9e
ToruNiina <niina.toru.68u@gmail.com>
2020-06-12 02:23:57 +0900

    :white_check_mark: add test_file_extension

---
+++ tests/util/test_file_extension.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE "test_util_file_extension"
+
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_NO_LIB
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include "util/file_extension.hpp"
+
+BOOST_AUTO_TEST_CASE(test_extension)
+{
+    using namespace std::literals::string_view_literals;
+
+    BOOST_TEST(mill::extension_of("hoge.txt"sv)  == ".txt"sv);
+    BOOST_TEST(mill::extension_of("piyo.dat"sv)  == ".dat"sv);
+    BOOST_TEST(mill::extension_of("fuga.xlsx"sv) == ".xlsx"sv);
+}
