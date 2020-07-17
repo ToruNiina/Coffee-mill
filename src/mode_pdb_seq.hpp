2eb0b4c678dddf8b5149bc9c827614b0daaa4935
ToruNiina <niina.toru.68u@gmail.com>
2020-07-17 15:15:08 +0900

    :recycle: simplify pdb seq mode

--- src/mode_pdb_seq.hpp
+++ src/mode_pdb_seq.hpp
@@ -16,103 +16,6 @@ inline const char* pdb_seq_usage() noexcept
            "      print sequences for each chain\n";
 }
 
-template<typename Iterator>
-struct residue_name_iterator
-{
-    residue_name_iterator(Iterator iter) : iter_(iter){}
-
-    std::string operator*() const
-    {
-        return remove_all(' ', iter_->residue_name());
-    }
-
-    residue_name_iterator& operator++()
-    {
-        ++iter_;
-        return *this;
-    }
-    residue_name_iterator& operator++(int)
-    {
-        const auto tmp = *this;
-        ++(*this);
-        return tmp;
-    }
-
-    Iterator iter_;
-};
-
-template<typename Iterator>
-inline residue_name_iterator<Iterator>
-wrap_residue_name(Iterator iter)
-{
-    return residue_name_iterator<Iterator>(iter);
-}
-
-template<typename Iterator>
-inline bool operator==(residue_name_iterator<Iterator> const& lhs,
-                       residue_name_iterator<Iterator> const& rhs)
-{
-    return lhs.iter_ == rhs.iter_;
-}
-
-template<typename Iterator>
-inline bool operator<(residue_name_iterator<Iterator> const& lhs,
-                      residue_name_iterator<Iterator> const& rhs)
-{
-    return lhs.iter_ < rhs.iter_;
-}
-
-template<typename Iterator>
-inline bool operator!=(residue_name_iterator<Iterator> const& lhs,
-                       residue_name_iterator<Iterator> const& rhs)
-{
-    return !(lhs == rhs);
-}
-
-template<typename Iterator>
-inline bool operator<=(residue_name_iterator<Iterator> const& lhs,
-                       residue_name_iterator<Iterator> const& rhs)
-{
-    return (lhs < rhs) || (lhs == rhs);
-}
-
-template<typename Iterator>
-inline bool operator>(residue_name_iterator<Iterator> const& lhs,
-                       residue_name_iterator<Iterator> const& rhs)
-{
-    return !(lhs <= rhs);
-}
-
-template<typename Iterator>
-inline bool operator>=(residue_name_iterator<Iterator> const& lhs,
-                       residue_name_iterator<Iterator> const& rhs)
-{
-    return !(lhs < rhs);
-}
-
-struct generate_sequence
-{
-    char operator()(const std::string& res) const
-    {
-        if(res.size() == 1)
-        {
-            return res.front();
-        }
-        else if(res.size() == 2 && res.at(0) == 'D')
-        {
-            return res.at(1);
-        }
-        else if(res.size() == 3 && amino_acid_3to1.count(res) != 0)
-        {
-            return amino_acid_3to1.at(res);
-        }
-        else
-        {
-            throw std::invalid_argument("unknown sequence: " + res);
-        }
-    }
-};
-
 // argv := {"seq", "file.pdb"}
 int mode_pdb_seq(int argument_c, const char **argument_v)
 {
@@ -134,10 +37,27 @@ int mode_pdb_seq(int argument_c, const char **argument_v)
     for(auto&& chain : reader.parse(reader.read(pdbname)))
     {
         std::string sequence(chain.size(), ' ');
-        std::transform(wrap_residue_name(chain.cbegin()),
-                       wrap_residue_name(chain.cend()),
-                       sequence.begin(),
-                       generate_sequence());
+        std::transform(chain.cbegin(), chain.cend(), sequence.begin(),
+            [](const auto& residue) -> char {
+                const auto resname = remove_all(' ', residue.residue_name());
+                if(resname.size() == 1)
+                {
+                    return resname.front();
+                }
+                else if(resname.size() == 2 && resname.at(0) == 'D')
+                {
+                    // assuming DA, DT, DC, DG
+                    return resname.at(1);
+                }
+                else if(resname.size() == 3 && amino_acid_3to1.count(resname) != 0)
+                {
+                    return amino_acid_3to1.at(resname);
+                }
+                else
+                {
+                    throw std::invalid_argument("unknown sequence: " + resname);
+                }
+            });
         std::cout << "chain " << chain.chain_id() << ": " << sequence << std::endl;
     }
     return 0;
