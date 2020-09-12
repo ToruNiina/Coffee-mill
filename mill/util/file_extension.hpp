c2904649b9b087c48dfa97752fc4ee55d2ebf41c
ToruNiina <niina.toru.68u@gmail.com>
2020-09-12 19:03:34 +0900

    :sparkles: add base_name_of(fname)

--- mill/util/file_extension.hpp
+++ mill/util/file_extension.hpp
@@ -19,5 +19,18 @@ inline std::string_view extension_of(const std::string_view filename)
     return filename.substr(dot, filename.size() - dot);
 }
 
+// the resulting string_view does NOT contains the period.
+inline std::string_view base_name_of(const std::string_view filename)
+{
+    const auto dot = filename.rfind('.');
+    if(dot == std::string_view::npos)
+    {
+        using namespace std::literals::string_view_literals;
+        log::error("no file extension in ", filename);
+        return ""sv;
+    }
+    return filename.substr(0, dot);
+}
+
 } // mill
 #endif// COFFEE_MILL_UTIL_FILE_EXTENSION_HPP
