52bd5bf29000e58dee1daf4c618675dff6b88a9f
ToruNiina <niina.toru.68u@gmail.com>
2020-10-16 16:40:16 +0900

    :recycle: split dcd info into hpp and cpp

---
+++ src/mode_dcd_info.cpp
@@ -0,0 +1,56 @@
+#include <mill/dcd/DCDReader.hpp>
+#include <mill/util/file_extension.hpp>
+#include "mode_dcd_info.hpp"
+
+namespace mill
+{
+
+const char* mode_dcd_info_usage() noexcept
+{
+    return "usage: mill dcd info <traj.dcd>\n"
+           "      dump header information of traj.dcd into stdout.\n"
+           "      you can check the size of traj, number of particles included,"
+           " and etc.\n";
+}
+
+int mode_dcd_info(std::deque<std::string_view> args)
+{
+    if(args.size() < 2)
+    {
+        log::error("mill dcd info: too few arguments");
+        log::error(mode_dcd_info_usage());
+        return 1;
+    }
+
+    const auto fname = args.at(1);
+    if(fname == "help")
+    {
+        log::info(mode_dcd_info_usage());
+        return 0;
+    }
+
+    if(extension_of(fname) == ".dcd")
+    {
+        DCDReader reader(fname);
+        const auto data = reader.read_header();
+
+        std::cout << "number of frames       : " << data.at("nset")       << std::endl;
+        std::cout << "initial step           : " << data.at("istart")     << std::endl;
+        std::cout << "interval between frames: " << data.at("nstep_save") << std::endl;
+        std::cout << "total step             : " << data.at("nstep")      << std::endl;
+        std::cout << "number of units        : " << data.at("nunit")      << std::endl;
+        std::cout << "number of particles    : " << data.at("nparticle")  << std::endl;
+        std::cout << "delta t                : " << data.at("delta_t")    << std::endl;
+        std::cout << "comment                : " << data.at("comment")    << std::endl;
+        return 0;
+    }
+    else
+    {
+        log::error("mill dcd info: unknown file extension ", fname);
+        log::error(mode_dcd_info_usage());
+        return 1;
+    }
+}
+
+} // mill
+
--- src/mode_dcd_info.hpp
+++ src/mode_dcd_info.hpp
@@ -1,59 +1,13 @@
-#ifndef COFFEE_MILL_DCD_INFO
-#define COFFEE_MILL_DCD_INFO
-#include <mill/dcd/DCDReader.hpp>
-
+#ifndef COFFEE_MILL_DCD_INFO_HPP
+#define COFFEE_MILL_DCD_INFO_HPP
 #include <string_view>
 #include <deque>
 
 namespace mill
 {
 
-inline const char* dcd_info_usage() noexcept
-{
-    return "usage: mill dcd info <traj.dcd>\n"
-           "      dump header information of traj.dcd into stdout.\n"
-           "      you can check the size of traj, number of particles included,"
-           " and etc.\n";
-}
-
-inline int mode_dcd_info(std::deque<std::string_view> args)
-{
-    if(args.size() < 2)
-    {
-        log::error("mill dcd info: too few arguments");
-        log::error(dcd_info_usage());
-        return 1;
-    }
-
-    const auto fname = args.at(1);
-    if(fname == "help")
-    {
-        log::info(dcd_info_usage());
-        return 0;
-    }
-
-    if(extension_of(fname) == ".dcd")
-    {
-        DCDReader reader(fname);
-        const auto data = reader.read_header();
-
-        std::cout << "number of frames       : " << data.at("nset")       << std::endl;
-        std::cout << "initial step           : " << data.at("istart")     << std::endl;
-        std::cout << "interval between frames: " << data.at("nstep_save") << std::endl;
-        std::cout << "total step             : " << data.at("nstep")      << std::endl;
-        std::cout << "number of units        : " << data.at("nunit")      << std::endl;
-        std::cout << "number of particles    : " << data.at("nparticle")  << std::endl;
-        std::cout << "delta t                : " << data.at("delta_t")    << std::endl;
-        std::cout << "comment                : " << data.at("comment")    << std::endl;
-        return 0;
-    }
-    else
-    {
-        log::error("mill dcd info: unknown file extension ", fname);
-        log::error(dcd_info_usage());
-        return 1;
-    }
-}
+const char* mode_dcd_info_usage() noexcept;
+int mode_dcd_info(std::deque<std::string_view> args);
 
 } // mill
 #endif  //COFFEE_MILL_DCD_INFO
