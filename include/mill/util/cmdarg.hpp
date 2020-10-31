c014d2a95b98b0f3788a8a475c52d5e7fd29f7ef
ToruNiina <niina.toru.68u@gmail.com>
2020-10-31 23:04:19 +0900

    :sparkles: add pop_arg without reading a value

--- include/mill/util/cmdarg.hpp
+++ include/mill/util/cmdarg.hpp
@@ -3,15 +3,26 @@
 #include <algorithm>
 #include <deque>
 #include <optional>
+#include <variant>
 #include <sstream>
 #include <string_view>
 
 namespace mill
 {
+namespace detail
+{
+template<typename T>
+struct argumnet_default_return_value{using type = T;};
+template<>
+struct argumnet_default_return_value<void>{using type = std::monostate;};
+
+template<typename T>
+using argumnet_default_return_value_t = typename argumnet_default_return_value<T>::type;
+} // detail
 
 // It pops `--arg=value` from `args`. It converts the `value` via istream.
-template<typename T>
-std::optional<T>
+template<typename T, typename U = detail::argumnet_default_return_value_t<T>>
+std::optional<U>
 pop_argument(std::deque<std::string_view>& args, std::string_view name)
 {
     const auto found = std::find_if(args.begin(), args.end(),
@@ -41,6 +52,31 @@ pop_argument(std::deque<std::string_view>& args, std::string_view name)
     return std::nullopt;
 }
 
+// It pops `--arg`. If exists, retval would have value.
+template<>
+inline std::optional<std::monostate>
+pop_argument<void, std::monostate>(std::deque<std::string_view>& args, std::string_view name)
+{
+    const auto found = std::find_if(args.begin(), args.end(),
+        [name](const auto arg) -> bool {
+            if(arg.size() != 2 + name.size())
+            {
+                return false;
+            }
+            if(arg.substr(0, 2) == "--" && arg.substr(2, name.size()) == name)
+            {
+                return true;
+            }
+            return false;
+        });
+
+    if(found != args.end())
+    {
+        args.erase(found);
+        return std::monostate{};
+    }
+    return std::nullopt;
+}
 
 } // mil
 #endif//COFFEE_MILL_UTIL_CMD_ARG_HPP
