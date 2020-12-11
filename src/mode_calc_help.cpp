e17654986b3ad70c0be15041af14ece9ab191637
ToruNiina <niina.toru.68u@gmail.com>
2020-12-11 18:14:41 +0900

    :children_crossing: add autocorrelation to calc help

--- src/mode_calc_help.cpp
+++ src/mode_calc_help.cpp
@@ -8,6 +8,7 @@
 #include "mode_calc_aabb.hpp"
 #include "mode_calc_obb.hpp"
 #include "mode_calc_center.hpp"
+#include "mode_calc_autocorrelation.hpp"
 
 namespace mill
 {
@@ -30,6 +31,8 @@ const char* mode_calc_help_usage() noexcept
            "      : construct OBB using covariances\n"
            "    - center\n"
            "      : calculate geometric center\n"
+           "    - autocorrelation\n"
+           "      : calculate autocorrelation of data\n"
            "    - help\n"
            "      : print detailed explanation of each command\n";
 }
@@ -73,6 +76,10 @@ int mode_calc_help(std::deque<std::string_view> args)
     else if(command == "center")
     {
         return mode_calc_center({"help"sv});
+    }
+    else if(command == "autocorrelation")
+    {
+        return mode_calc_autocorrelation({"help"sv});
     }
     else if(command == "help")
     {
