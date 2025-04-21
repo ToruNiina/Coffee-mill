fe6b640cd4907583985a41e1a02f1d4919e3f4a0
ToruNiina <niina.toru.68u@gmail.com>
2025-04-22 00:23:38 +0900

    feat: add toml file mode to calc angle

--- src/mode_calc_angle.cpp
+++ src/mode_calc_angle.cpp
@@ -18,7 +18,16 @@ const char* mode_calc_angle_usage() noexcept
            "       - NOTE THAT HERE IT CALCULATES THE ANGLE FORMED BY i->j and k->l VECTORS.\n"
            "       - output will be printed on stdout.\n"
            "    3. mill calc angle [x y z] [x y z]\n"
-           "       - It just calculate angle formed by the two vectors.\n";
+           "       - It just calculate angle formed by the two vectors.\n"
+           "    4. mill calc angle [input.toml]\n"
+           "       ```toml\n"
+           "       traj = \"traj1.dcd\"\n"
+           "       angles = [\n"
+           "           [ [i0: int, j0: int ], [k0: int, l0: int ] ], \n"
+           "           [ [i1: int, j1: int ], [k1: int, l1: int ] ], \n"
+           "           ... , \n"
+           "       ]\n"
+           "       ```\n";
 }
 
 int mode_calc_angle(std::deque<std::string_view> args)
@@ -39,7 +48,30 @@ int mode_calc_angle(std::deque<std::string_view> args)
 
     constexpr double rad_to_deg = 180.0 / constants::pi;
 
-    if(args.size() == 4) // traj.dcd i j k
+    if(args.size() == 1) // input.toml
+    {
+        const std::string fname(args.front());
+        args.pop_front();
+
+        const auto input  = toml::parse(fname);
+        const auto traj   = toml::find<std::string>(input, "traj");
+        const auto vecs = toml::find<std::vector<
+            std::pair<std::array<std::size_t, 2>, std::array<std::size_t, 2>>
+            >>(input, "angles");
+
+        for(const auto& frame : reader(traj))
+        {
+            for(const auto& [vec1, vec2] : vecs)
+            {
+                const auto [i, j] = vec1;
+                const auto [k, l] = vec2;
+                const auto v1 = frame[i].position() - frame[j].position();
+                const auto v2 = frame[k].position() - frame[l].position();
+                std::cout << std::setprecision(16) << angle(v1, v2) * rad_to_deg << '\n';
+            }
+        }
+    }
+    else if(args.size() == 4) // traj.dcd i j k
     {
         const auto i = std::stoi(std::string(args.at(1)));
         const auto j = std::stoi(std::string(args.at(2)));
